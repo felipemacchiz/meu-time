b714ae878ec05f122efa2cffd134545dcd52e3cd
felipemacchiall-dev14 <felipe.macchi@allstrategy.com.br>
2023-05-19 14:26:24 -0300

    Estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+      <>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </>
+    );
 };
 
 export default App;
